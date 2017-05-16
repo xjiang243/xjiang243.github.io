45b28eed4139c1b8ac9888e8e2fda74e3688a66c
xjiang243 <xjiang243@gmail.com>
2017-05-15 20:39:58 -0500

    first add

---
+++ scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name footerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the footerApp
+ */
+angular.module('profile-App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
